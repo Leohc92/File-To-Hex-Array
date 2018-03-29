7f80a123ce4925859d4024cd76be36c6fec3dc0a
Chenyu Wu <chenyu.wu@outlook.com>
2018-03-29 13:32:29 +0800

    update for specific usage
    convert bin file to a file called "ecu_iod_ble_binFile.c"

--- main.cpp
+++ main.cpp
@@ -3,49 +3,56 @@
 #include <fstream>
 #include <string>
 
-
+int i;
 int main(int argc, char *argv[]) {
-	if (argc != 4) {
-		std::cerr << "Usage: " << argv[0] << " <file> <header_name> <varname>" << std::endl;
+	if (argc != 2) {
+        std::cerr <<"Format Error!"<<std::endl;
+		std::cerr << "Format: " << argv[0] << " <bin_file_name>" << std::endl;
 		return 1;
 	}
 
 	const std::string file = argv[1];
-	const std::string header = argv[2];
-	const std::string var = argv[3];
 
 	std::fstream input(file, std::ios_base::in | std::ios_base::binary);
 	if (!input.is_open()) {
-		std::cerr << "Input file is not readable" << std::endl;
+		std::cerr << "Input file Error" << std::endl;
 		return 1;
 	}
 
 	std::string content((std::istreambuf_iterator<char>(input)), (std::istreambuf_iterator<char>()));
 	if (content.length() == 0) {
-		std::cerr << "Input file has no data" << std::endl;
+		std::cerr << "Input file is empty" << std::endl;
 		return 1;
 	}
 
-	std::fstream headerFile(header, std::ios_base::out);
+	std::fstream headerFile("ecu_iod_ble_binFile.c", std::ios_base::out);
 	if (!headerFile.is_open()) {
-		std::cerr << "Header file ist not writable" << std::endl;
+		std::cerr << "C file ist not writable" << std::endl;
 		return 1;
 	}
 
 
-	headerFile << "#pragma once" << std::endl << std::endl;
-	headerFile << " const unsigned char " << var << "[] = {";
+	headerFile << "/* This C file is auto generated. */" << std::endl<<std::endl;
+	headerFile << "#include \"ecu_iod_ble_sbllib.h\"" <<std::endl;
+	headerFile << "#include \"ecu_iod_ble_serial.h\"" <<std::endl;
+	headerFile << "#include \"ecu_iod_ble_binFile.h\""<<std::endl<<std::endl;
+
+
+
+	headerFile << "const uint8_t fileBuffer[FILE_BUFFER_SIZE]={";
 	{
-		int i = 0;
+		i = 0;
 		for (auto it = content.begin(); it != content.end(); it++, i++) {
-			if (i % 10 == 0)
+			if (i % 16 == 0)
 				headerFile << std::endl << "\t";
 
-			headerFile << "0x" << std::hex << (((unsigned int)(*it)) & 0xff);
+			headerFile << "0x" << std::hex <<std::setfill('0') << std::setw(2) << std::uppercase << (((unsigned int)(*it)) & 0xFF);
 			if (std::next(it) != content.end())
-				headerFile << ",";
+				headerFile << ", ";
 		}
 	}
 	headerFile << std::endl << "};" << std::endl;
+	std::cout<<"Convert completed."<<std::endl;
+	std::cout<<"Total: "<<i<<" bytes."<<std::endl;
 	return 0;
 }
